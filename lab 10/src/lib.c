70a357632fd60c84730434dc6b2a971e1db01430
Lordpluha <Tesluakevlad@gmail.com>
2024-05-12 15:46:20 +0200

    Complete lab 10

---
+++ lab 10/src/lib.c
@@ -0,0 +1,14 @@
+#include "lib.h"
+
+void sort(int *arr, int n) {
+    // Сортування масиву методом бульбашки (bubble sort)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
---
+++ lab 10/src/lib.h
@@ -0,0 +1,9 @@
+#ifndef SAMPLE_PROJECT_LIB_H 
+#define SAMPLE_PROJECT_LIB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int *arr, int n);
+
+#endif /* LIB_H */
